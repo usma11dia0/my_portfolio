2837ec7c2fd89b88ac9fcad45a4a6362af7e5a10
usma11dia0 <usma11dia0@gmail.com>
2024-01-28 15:33:51 +0900

    add: attach tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+  content: [ 
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -13,3 +13,4 @@ module.exports = {
   },
   plugins: [],
 }
+
