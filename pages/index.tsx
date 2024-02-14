9e7cabcc737a06401d3226a3b778885c84f11ff6
usma11dia0 <usma11dia0@gmail.com>
2024-02-14 23:50:18 +0900

    add: MobileNav.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import MobileNav from '@/Components/MobileNav';
 import Nav from '@/Components/Nav';
 import React, { useState } from 'react'
 
@@ -5,12 +6,13 @@ const HomePage = () => {
 
   const [nav, setNav] = useState(false)
   const openNav = () => setNav(true)
-  const closeNav = () => setNav(true)
+  const closeNav = () => setNav(false)
 
   return (
     <div className="overflow-x-hidden">
       <div>
-        <Nav />
+        <MobileNav nav={nav} closeNav={closeNav}/>
+        <Nav openNav={openNav}/>
       </div>
     </div>
   );
