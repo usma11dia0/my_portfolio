b8ecbd8bcc7404bd59c50db49fa0e50955a8dd52
usma11dia0 <usma11dia0@gmail.com>
2024-03-14 11:45:11 +0900

    fix: tmp ImageMotion.tsx

--- components/projects/ImageMotion.tsx
+++ components/projects/ImageMotion.tsx
@@ -5,14 +5,15 @@ export default function App() {
     const [isMounted, setIsMounted] = React.useState(false);
   
     const defaultState = {
-        opacity: 0,
-        scaleX: 0, // 横方向のスケールを0に設定
-        width: "100%", // コンテナの幅に合わせる
-        height: "100px",
-      };
+      opacity: 0,
+      scaleX: 0,
+      scaleY: 0,
+      width: "100%",
+      height: "100px",
+    };
   
     return (
-      <div className="container" style={{position: "relative", height: "200px"}}>
+      <div className="container" style={{ position: "relative", height: "200px" }}>
         <button onClick={() => setIsMounted(!isMounted)}>
           {`${isMounted ? "Unmount" : "Mount"} Element`}
         </button>
@@ -21,22 +22,30 @@ export default function App() {
             <motion.div
               className="block"
               initial={defaultState}
-              exit={defaultState}
+              exit={{
+                opacity: 0,
+                scaleX: 1, // アンマウント時には元のサイズに戻す
+                scaleY: 1, // アンマウント時には元のサイズに戻す
+                transition: { duration: 0.3 } // アンマウント時のアニメーションの持続時間を設定
+              }}
               animate={{
                 opacity: 1,
+                scaleX: [0, 1.5, 0.3, 1, 1], // 最初に横に伸び、その後元に戻る
+                scaleY: [0, 0.02, 1.3, 1.8, 1], // scaleYのアニメーションはscaleXの後に実行
                 boxShadow: "1px 1px 10px rgba(0, 0, 0, 0.3)",
-                scaleX: 1,
               }}
               transition={{
-                duration: 0.1, // アニメーションの持続時間
-                ease: "easeInOut", // アニメーションのイージング関数
+                duration: 0.4,
+                ease: "easeIn",
+                scaleX: { duration: 0.3, times: [0, 0.6, 0.65, 0.75, 1] }, // scaleXのアニメーションタイミング
+                scaleY: { duration: 0.3, times: [0, 0.6, 0.65, 0.75, 1] }, // scaleYのアニメーションタイミング
               }}
               style={{
-                position: "absolute", // 要素の位置を絶対位置に設定
-                top: "50%", // 上辺をコンテナの中央に設定
-                left: "50%", // 左辺をコンテナの中央に設定
-                x: "-50%", // X軸に沿って要素を中央にシフト
-                y: "-50%", // Y軸に沿って要素を中央にシフト
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                x: "-50%",
+                y: "-50%",
               }}
             />
           )}
