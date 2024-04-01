acc962f0c5b47d3ba1ac2415dd357d4dc3464e50
usma11dia0 <usma11dia0@gmail.com>
2024-04-01 09:40:09 +0900

    fix:remove Head component from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import {NextIntlClientProvider} from 'next-intl';
 import {useRouter} from 'next/router';
 
 import { bleepsSettings } from "./_appConfig";
-import { Head } from "next/document";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
